b4b86632105e854adc282fa8a9defb396c26bb10
axumrs <rolson867@gmail.com>
2021-11-15 19:19:37 +0800

    将结构体字段定义为Option并填充到结构体中

--- request/src/main.rs
+++ request/src/main.rs
@@ -42,12 +42,26 @@ async fn subject(Query(args): Query<SubjectArgs>) -> String {
     format!("Page {}, keyword: {} of subjects", args.page, args.keyword)
 }
 
+/// 把 Query 参数变成Option
 async fn subject_opt(args: Option<Query<SubjectArgs>>) -> String {
     if let Some(args) = args {
         let args = args.0;
         return format!("Page {}, keyword: {} of subjects", args.page, args.keyword);
     }
     "Page 0, no keyword of subjects".to_string()
+}
+
+#[derive(Deserialize)]
+pub struct SubjectArgsOpt {
+    pub page: Option<i32>,
+    pub keyword: Option<String>,
+}
+/// 将结构体的字段变成Option，并将参数填充到结构体
+async fn subject_opt_done(Query(args): Query<SubjectArgsOpt>) -> String {
+    let page = args.page.unwrap_or(0);
+    let keyword = args.keyword.unwrap_or("".to_string());
+
+    format!("Page {}, keyword: {} of subjects", page, keyword)
 }
 
 #[tokio::main]
@@ -57,7 +71,8 @@ async fn main() {
         .route("/repo/:user/:repo", get(repo_info))
         .route("/repo_struct/:user_name/:repo_name", get(repo_info_struct))
         .route("/subject", get(subject))
-        .route("/subject_opt", get(subject_opt));
+        .route("/subject_opt", get(subject_opt))
+        .route("/subject_opt_done", get(subject_opt_done));
     axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
         .serve(app.into_make_service())
         .await
