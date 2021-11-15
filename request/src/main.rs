28651b40546f4700d867f1ea2b9fbaed6089666f
axumrs <rolson867@gmail.com>
2021-11-15 18:23:44 +0800

    将Path参数填充到结构体

--- request/src/main.rs
+++ request/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{extract::Path, routing::get, Router};
+use serde::Deserialize;
 
 /// 获取单个 Path 参数
 async fn user_info(Path(id): Path<i32>) -> String {
@@ -13,11 +14,26 @@ async fn repo_info(Path((user_name, repo_name)): Path<(String, String)>) -> Stri
     )
 }
 
+#[derive(Deserialize)]
+pub struct RepoInfo {
+    pub user_name: String,
+    pub repo_name: String,
+}
+
+/// 将 Path 参数填充到结构体
+async fn repo_info_struct(Path(info): Path<RepoInfo>) -> String {
+    format!(
+        "Repository: user name: {} and repository name: {}",
+        info.user_name, info.repo_name
+    )
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/user/:id", get(user_info))
-        .route("/repo/:user/:repo", get(repo_info));
+        .route("/repo/:user/:repo", get(repo_info))
+        .route("/repo_struct/:user_name/:repo_name", get(repo_info_struct));
     axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
         .serve(app.into_make_service())
         .await
