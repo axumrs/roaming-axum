7ce52bf0ad1b4c42e571a9e659d0c5d1ddd93391
axumrs <rolson867@gmail.com>
2021-11-15 16:12:43 +0800

    纯文本之String

--- response/src/main.rs
+++ response/src/main.rs
@@ -2,12 +2,17 @@ use axum::routing::get;
 use axum::Router;
 
 async fn str_response() -> &'static str {
-    "你好，axum.rs"
+    "Hello, axum.rs"
+}
+async fn string_response() -> String {
+    "Hello, axum.rs".to_string()
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/str", get(str_response));
+    let app = Router::new()
+        .route("/str", get(str_response))
+        .route("/string", get(string_response));
     axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
         .serve(app.into_make_service())
         .await
