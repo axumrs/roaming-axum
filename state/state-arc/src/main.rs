cc6df2bd2a12c858b71d6ae7fa7a1ced17f2cc1d
axumrs <rolson867@gmail.com>
2021-11-17 14:10:43 +0800

    使用Arc状态共享

--- state/state-arc/src/main.rs
+++ state/state-arc/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Extension, routing, AddExtensionLayer, Router};
+use std::sync::Arc;
+
+pub struct UserInfo {
+    pub username: String,
+}
+async fn show_user_info(Extension(info): Extension<Arc<UserInfo>>) -> String {
+    format!("Sigined User: {}", info.username)
+}
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/user", routing::get(show_user_info))
+        .layer(AddExtensionLayer::new(Arc::new(UserInfo {
+            username: "axum.rs".to_string(),
+        })));
+    axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
