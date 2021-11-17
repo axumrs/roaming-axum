34a500e7cc2c32b3aa45f3b246d60d6ac46a0811
axumrs <rolson867@gmail.com>
2021-11-17 14:21:10 +0800

    复合共享数据

--- state/state-compound/src/main.rs
+++ state/state-compound/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Extension, routing, AddExtensionLayer, Router};
+
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct DatabaseClient {
+    pub dsn: String,
+}
+pub struct RedisClient {
+    pub host: String,
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: DatabaseClient,
+    pub rdb: Arc<RedisClient>,
+}
+
+async fn status(Extension(state): Extension<AppState>) -> String {
+    format!(
+        "database dsn: {}, redis host: {}",
+        state.db.dsn, state.rdb.host
+    )
+}
+#[tokio::main]
+async fn main() {
+    let db_client = DatabaseClient {
+        dsn: "host=pg.axum.rs port=5432 user=axum_rs password=axum.rs sslmode=disable".to_string(),
+    };
+    let redis_client = Arc::new(RedisClient {
+        host: "redis.axum.rs".to_string(),
+    });
+
+    let app = Router::new()
+        .route("/status", routing::get(status))
+        .layer(AddExtensionLayer::new(AppState {
+            db: db_client,
+            rdb: redis_client,
+        }));
+    axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
