6c0a4db34a24fa5525ebbc4d04106d7c282120cd
axumrs <rolson867@gmail.com>
2021-11-17 17:47:48 +0800

    使用中间件处理静态资源

--- static-files/src/main.rs
+++ static-files/src/main.rs
@@ -1,12 +1,26 @@
-use axum::{routing, Router};
+use axum::error_handling::HandleErrorExt;
+use axum::http::StatusCode;
+use axum::routing::service_method_routing as service;
+use axum::Router;
+use tower_http::services::ServeDir;
 
+#[allow(unused)]
 async fn axum_rs_txt() -> String {
     std::fs::read_to_string("static/axum-rs.txt").unwrap()
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/static/axum-rs.txt", routing::get(axum_rs_txt));
+    //let app = Router::new().route("/static/axum-rs.txt", axum::routing::get(axum_rs_txt));
+    let app = Router::new().nest(
+        "/static",
+        service::get(ServeDir::new("static")).handle_error(|err| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("处理静态资源出错：{:?}", err),
+            )
+        }),
+    );
     axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
         .serve(app.into_make_service())
         .await
